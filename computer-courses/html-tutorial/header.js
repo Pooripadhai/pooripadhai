63f9818e3e8ef3fd4aca72908012b3b886163586
Poori Padhai <pooripadhai@gmail.com>
2018-07-27 00:01:54 +0530

    HTML New Cource

---
+++ computer-courses/html-tutorial/header.js
@@ -0,0 +1,10 @@
+function course_Header()
+{
+	
+document.write("<link rel='SHORTCUT ICON' href='http://www.pooripadhai.com/favicon.png'><link rel='stylesheet' href='http://www.pooripadhai.com/css/pp-css.css'><link rel='stylesheet' href='http://www.pooripadhai.com/css/font-awesome-4.7.0/css/font-awesome.min.css'>");
+}
+
+function base_Url()
+{
+	document.write("http://www.pooripadhai.com");
+}
